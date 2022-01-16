4681e3f748958f55dd6cf42638a903609721e4d9
Wouter Roosendaal <tuvokki@gmail.com>
2022-01-16 21:53:49 +0100

    Test commit 0041

--- asok/__init__.py
+++ asok/__init__.py
@@ -12,7 +12,7 @@ def root_factory(request):
 def main(global_config, **settings):
     """
     This function returns a Pyramid WSGI application.
-    This is a change to make a test commit 004
+    This is a change to make a test commit 0041
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
