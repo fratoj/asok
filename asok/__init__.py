64ef8381372114c307e728c8a4297955a1a69f02
Wouter Roosendaal <tuvokki@gmail.com>
2022-01-16 17:51:30 +0100

    Update prod settings

--- asok/__init__.py
+++ asok/__init__.py
@@ -12,7 +12,7 @@ def root_factory(request):
 def main(global_config, **settings):
     """
     This function returns a Pyramid WSGI application.
-    This is a change to make a test commit 002
+    This is a change to make a test commit 003
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
